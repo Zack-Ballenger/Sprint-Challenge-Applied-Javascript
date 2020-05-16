de89d70ef25a79fae9cca2568cdc9e42f1aabb16
Zack-Ballenger <zack.ballenger@tutanota.com>
2020-05-16 11:02:21 -0600

    making decent progress, almost done with header

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,31 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header(titles, dates, temperature) {
+
+const header = document.createElement('div');
+const date = document.createElement('span');
+const temp = document.createElement('span');
+const h1 = document.createElement('div');
+
+header.classList.add('header');
+date.classList.add('date');
+temp.classList.add('temp');
+
+header.append(date, h1, temp);
+
+date.textContent = dates;
+temp.textContent = temperature;
+h1.textContent = titles;
+
+
+return header;
+
+
+}
+
+
+const headerContainer = document.querySelector('.header-container')
+
+headerContainer.appendChild(Header("Ayyyyyyyyyyy", "Lambda", "65"))
+
