d740186e8b139874bb0f7c904170263fcd640427
Zack-Ballenger <zack.ballenger@tutanota.com>
2020-05-16 10:46:29 -0600

    finished with first section

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -20,5 +20,68 @@
 // Use your function to create a card for each of the articles and add the card to the DOM.
 const cardEntry = document.querySelector(".cards-container");
 
+// axios.get("https://lambda-times-backend.herokuapp.com/articles")
+//     .then(res => {
+//         console.log(res);
+//         const articleValues = Object.values(response.data.articles);
+//         console.log(articleValues);
+//         articleValues.forEach(item => {
+//             item.forEach(i => {
+//                 const newArticle = CardMaker(i);
+//                 cardEntry.appendChild(newArticle);
+//             });
+//         });
+//     });
 
-axios.get
+function Card(headline, url, name) {
+    const img = document.createElement('img')
+    const headLine = document.createElement('div')
+    const imgContainer = document.createElement('div')
+    const author = document.createElement('span')
+    const card = document.createElement('div')
+    const authorContainer = document.createElement('div')
+
+
+
+
+    card.classList.add('card')
+    headLine.classList.add('headline')
+    authorContainer.classList.add('author')
+    imgContainer.classList.add('img-container')
+  
+    headLine.textContent = headline
+    img.src = url
+    author.innerHTML = `By ${name}`
+  
+    imgContainer.appendChild(img)
+    authorContainer.append(imgContainer, author)
+    card.append(headLine, authorContainer)
+  
+    return card
+  }
+  
+  axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(res => {
+      console.log(res);
+      const data = res.data.articles
+      articleTypes = [
+        data.javascript,
+        data.bootstrap,
+        data.technology,
+        data.jquery,
+        data.nod
+      ]
+      console.log(articleTypes);
+    })
+    .then(() => {
+      const cardContainer = document.querySelector('.cards-container')
+      articleTypes.forEach(articles => {
+        articles.forEach(article => {
+          cardContainer.append(
+            Card(article.headline, article.authorPhoto, article.authorName)
+          )
+        }
+        )
+      })
+    })
+    .catch(err => console.log(err))
